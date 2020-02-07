450dd0524af8c273255b25b5d915247eab0994dd
jake612 <jake612@live.unc.edu>
2020-02-07 14:49:57 -0500

    Updated help method to properly handle --help and -h while also printing correctly

--- src/idiot.clj
+++ src/idiot.clj
@@ -4,9 +4,9 @@
   "Function to print help messages for a function"
   [function]
   (cond
-    (= "idiot" function) (prn "idiot: the other stupid content tracker\n\nUsage: idiot <command> [<args>]\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
-    (= "help" function) (prn "idiot help: print help for a command\n\nUsage: idiot help <command>\n\nArguments:\n   <command>   the command to print help for\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
-    :else (prn "Error: invalid command\n")))
+    (or (nil? function) (= "idiot" function)) (print "idiot: the other stupid content tracker\n\nUsage: idiot <command> [<args>]\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
+    (or (= "-h" function) (= "--help" function))  (print "idiot help: print help for a command\n\nUsage: idiot help <command>\n\nArguments:\n   <command>   the command to print help for\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
+    :else (print "Error: invalid command\n")))
 
 (defn -main
   "Main method for handling CLI"
@@ -16,5 +16,5 @@
     (cond
       (or (= num-args 0) (= command "-h") (= command "--help")) (help "idiot")
       (= command "help") (help (second args))
-      )
+      :else (print "Error: invalid command\n"))
     ))
