fc67c326ce4aea950302522aec293ae6e398f896
jake612 <jake612@live.unc.edu>
2020-02-12 17:10:01 -0500

    reduced redundancy in determining help message

---
+++ src/cat.clj
@@ -0,0 +1,5 @@
+(ns cat)
+
+(defn cat-file
+  "function for handling cat-file command"
+  [])
--- src/idiot.clj
+++ src/idiot.clj
@@ -1,6 +1,7 @@
 (ns idiot
   (:require [clojure.java.io :as io])
-  (:require [hash :as hash]))
+  (:require hash)
+  (:require cat))
 
 (defn help
   "Function to print help messages for a function"
@@ -10,6 +11,7 @@
     (or (= "-h" function) (= "--help" function) (= "help" function))  (println "idiot help: print help for a command\n\nUsage: idiot help <command>\n\nArguments:\n   <command>   the command to print help for\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
     (= "init" function) (println "idiot init: initialize a new database\n\nUsage: idiot init\n\nArguments:\n   -h   print this message")
     (= "hash-object" function) (println "idiot hash-object: compute address and maybe create blob from file\n\nUsage: idiot hash-object [-w] <file>\n\nArguments:\n   -h       print this message\n   -w       write the file to database as a blob object\n   <file>   the file")
+    (= "cat-file" function) (println "idiot cat-file: print information about an object\n\nUsage: idiot cat-file -p <address>\n\nArguments:\n   -h          print this message\n   -p          pretty-print contents based on object type\n   <address>   the SHA1-based address of the object")
     :else (println "Error: invalid command")))
 
 (defn init
@@ -27,15 +29,15 @@
   "Main method for handling CLI"
   [& args]
   (let [num-args (count args)
-        command (first args)]
+        command (first args)
+        check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
+                                         (help command)
+                                         (func (rest args))))]
     (cond
       (or (= num-args 0) (= command "-h") (= command "--help")) (help "idiot")
       (= command "help") (help (second args))
-      (= command "init") (if (or (= "-h" (second args)) (= "--help" (second args)))
-                           (help "init")
-                           (init (rest args)))
-      (= command "hash-object") (if (or (= "-h" (second args)) (= "--help" (second args)))
-                                  (help "hash-objects")
-                                  (hash/hash-object (rest args)))
+      (= command "init") (check-first init)
+      (= command "hash-object") (check-first hash/hash-object)
+      (= command "cat-file") (check-first cat/cat-file)
       :else (println "Error: invalid command"))
     ))
