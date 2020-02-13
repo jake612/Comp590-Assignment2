a9e452085bceb7dff2830895dacd24710594a01a
jake612 <jake612@live.unc.edu>
2020-02-12 22:51:40 -0500

    small style tweak

--- src/idiot.clj
+++ src/idiot.clj
@@ -21,9 +21,9 @@
     (or (= "--help" (first args)) (= "-h" (first args))) (help "init")
     (> (count args) 0) (println "Error: init accepts no arguments")
     :else (if (.isDirectory (io/file ".git"))
-                  (println "Error: .git directory already exists")
-                  (do (io/make-parents ".git/objects/sample.txt")
-                      (println "Initialized empty Idiot repository in .git directory")))))
+            (println "Error: .git directory already exists")
+            (do (io/make-parents ".git/objects/sample.txt")
+                (println "Initialized empty Idiot repository in .git directory")))))
 
 (defn -main
   "Main method for handling CLI"
