2d3a676d4d021c2a89b685ec199c748006107bac
jake612 <jake612@live.unc.edu>
2020-03-02 19:23:25 -0500

    revert

--- src/idiot.clj
+++ src/idiot.clj
@@ -8,11 +8,13 @@
             [commit-tree]
             [file-io :as fio]))
 
-(defn leftovers
-  [args dir db command handle-main-switches]
-  (let [ check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
-                                  (help/help command)
-                                  (func (rest args) dir db)))
+(defn handle-main-switches
+  [args dir db]
+  (let [num-args (count args)
+        command (first args)
+        check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
+                                 (help/help command)
+                                 (func (rest args) dir db)))
         handle-r-switch (fn [[targetdir & rargs]] (cond
                                                     (nil? targetdir) (println "Error: the -r switch needs an argument")
                                                     (not (.exists (io/as-file targetdir))) (println "Error: the directory specified by -r does not exist")
@@ -22,16 +24,7 @@
                                                    :else (handle-main-switches rargs dir targetdb)))]
     (cond
       (= command "-r") (handle-r-switch (rest args))
-      (= command "-d") (handle-d-switch (rest args)))))
-
-(defn handle-main-switches
-  [args dir db]
-  (let [num-args (count args)
-        command (first args)
-        check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
-                                 (help/help command)
-                                 (func (rest args) dir db)))]
-    (cond
+      (= command "-d") (handle-d-switch (rest args))
       (or (= num-args 0) (= command "-h") (= command "--help")) (help/help "idiot")
       (= command "help") (help/help (second args))
       (= command "init") (check-first init/init)
@@ -45,4 +38,3 @@
   "Main method for handling CLI"
   [& args]
   (handle-main-switches args ".\\" ".idiot"))
-
