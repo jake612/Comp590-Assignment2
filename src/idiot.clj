3ce6d6ad43dcc4e75a7944f95905a021981499e4
jake612 <jake612@live.unc.edu>
2020-03-02 19:21:08 -0500

    init

--- src/idiot.clj
+++ src/idiot.clj
@@ -8,6 +8,22 @@
             [commit-tree]
             [file-io :as fio]))
 
+(defn leftovers
+  [args dir db command handle-main-switches]
+  (let [ check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
+                                  (help/help command)
+                                  (func (rest args) dir db)))
+        handle-r-switch (fn [[targetdir & rargs]] (cond
+                                                    (nil? targetdir) (println "Error: the -r switch needs an argument")
+                                                    (not (.exists (io/as-file targetdir))) (println "Error: the directory specified by -r does not exist")
+                                                    :else (handle-main-switches rargs (str targetdir "\\") db)))
+        handle-d-switch (fn [[targetdb & rargs]] (cond
+                                                   (nil? targetdb) (println "Error: the -d switch needs an argument")
+                                                   :else (handle-main-switches rargs dir targetdb)))]
+    (cond
+      (= command "-r") (handle-r-switch (rest args))
+      (= command "-d") (handle-d-switch (rest args)))))
+
 (defn handle-main-switches
   [args dir db]
   (let [num-args (count args)
@@ -30,17 +46,3 @@
   [& args]
   (handle-main-switches args ".\\" ".idiot"))
 
-(defn leftovers
-  (let [ check-first (fn [func] (if (or (= "-h" (second args)) (= "--help" (second args)))
-                                         (help/help command)
-                                         (func (rest args) dir db)))
-        handle-r-switch (fn [[targetdir & rargs]] (cond
-                                                    (nil? targetdir) (println "Error: the -r switch needs an argument")
-                                                    (not (.exists (io/as-file targetdir))) (println "Error: the directory specified by -r does not exist")
-                                                    :else (handle-main-switches rargs (str targetdir "\\") db)))
-        handle-d-switch (fn [[targetdb & rargs]] (cond
-                                                   (nil? targetdb) (println "Error: the -d switch needs an argument")
-                                                   :else (handle-main-switches rargs dir targetdb)))]
-    (cond
-      (= command "-r") (handle-r-switch (rest args))
-      (= command "-d") (handle-d-switch (rest args)))))
